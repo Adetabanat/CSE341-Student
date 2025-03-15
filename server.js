6e32c0a478825914b468a8dbfa82e5603fdd53b0
AdongoNat <danieladongonat@gmail.com>
2025-03-15 18:40:13 +0000

    controllers

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const exprees = require('express');
+const express = require('express');
 const app = express();
-const mongodb = require('./data/database');
 
-const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
+const dotenv = require('dotenv');
+
+
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json()); 
+
+app.use('students', require('./routes/students'));
+
+mongodb.iniDb = ((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`connnected to a  Database and Listing on port ${port}`);
+        }
+        )
+    }
+})
