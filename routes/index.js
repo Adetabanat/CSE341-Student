aa0c29c88e452c7c7a91fc3c33304944826c4ad8
AdongoNat <danieladongonat@gmail.com>
2025-03-17 18:49:44 +0000

    SMS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 
 router.get("/", (req, res) => {
-    res.send("Welcome to my Students Records API");
+    res.send("Welcome to my SMS Records API");
 });
 
 router.use('/students', require('./students'));
