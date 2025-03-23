688a6549532e1948489e17154bc78b95952313c4
AdongoNat <danieladongonat@gmail.com>
2025-03-23 19:56:44 +0000

    correction

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,8 +1,9 @@
 const isAuthenticated = (req, res, next) => {
-    if (!req.session.user === undefined) {
-        return res.status(401).json({ message: 'Unauthorized: Please log in.' });
+    if (!req.session.user == undefined) {  // Correctly checking if user is not set
+        return res.status(401).json({ message: 'You dont not have access: Please log in.' });
     }
     next();
 };
 
 module.exports = { isAuthenticated };
+
--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  swaggerOptions: {
+    oauth2RedirectUrl: "https://cse341-student.onrender.com/api-docs/oauth2-redirect.html",
+    oauth: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      appName: "Student Records API"
+    }
+  }
+}));
 
 module.exports = router;
