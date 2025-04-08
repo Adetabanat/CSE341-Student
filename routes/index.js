621761a6139fd6bd7218e78de9ea2f4cf7b2964a
AdongoNat <danieladongonat@gmail.com>
2025-04-08 18:42:23 +0000

    render

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,7 @@ router.use("/users", require("./users"));
 // GitHub OAuth login
 router.get("/login", passport.authenticate("github", { scope: ["user:email"] }));
 
-// GitHub OAuth callback (already handled in server.js, so this may not be needed here)
-// You can remove this if it's already in `server.js`, otherwise include it if you want all routing logic here.
-/*
-router.get("/github/callback",
-    passport.authenticate("github", { failureRedirect: "/api-docs" }),
-    (req, res) => {
-        res.redirect("/profile");
-    }
-);
-*/
 
-// Logout route
 router.get("/logout", (req, res, next) => {
     req.logout(err => {
         if (err) return next(err);
@@ -39,7 +28,7 @@ router.get("/logout", (req, res, next) => {
 router.get("/", (req, res) => {
     res.send(req.isAuthenticated() 
         ? `Logged in as ${req.user.displayName || req.user.username}` 
-        : "Welcome to the API. Please log in at /login");
+        : "Welcome to the API. Please log in ");
 });
 
 module.exports = router;
