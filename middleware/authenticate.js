0b6a0d59a8f75f10b1cb2f0adcdb2a95b3ea9a7b
AdongoNat <danieladongonat@gmail.com>
2025-04-09 17:28:42 +0000

    sma

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,10 @@
-const isAuthenticated = (req, res, next) =>{
-    if(req.session.user === undefined){
-        return res.status(401).json("🔒 Unauthorized. Please log in.");
+const isAuthenticated = (req, res, next) => {
+    if (!req.session.user) {
+      return res.status(401).json("🔒 Unauthorized. Please log in.");
     }
     next();
-};
-
+  };
+  
 module.exports = {
     isAuthenticated
 }
